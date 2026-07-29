7579ee53c073c8f21cb992620190f8c1530bd233
LipIndex <lipeng@yuanfeihd.com>
2026-07-29 11:47:37 +0800

    fix: 删除垃圾

--- autoUpdate/autoUpdate.js
+++
@@ -1,9 +0,0 @@
-
-const electron = require('electron');
-
-electron.app.on('ready', () => {
-    electron.autoUpdater.setFeedURL({
-        url: 'http://local-server/update.json'
-    });
-    electron.autoUpdater.checkForUpdates();
-});
